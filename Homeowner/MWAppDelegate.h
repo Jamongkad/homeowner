10ac6d7ee35539db3d1b3cc86695882c1f585373
jamongkad <wrm932@gmail.com>
2014-08-24 18:36:14 +0800

    Initial Commit

---
+++ Homeowner/MWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWAppDelegate.h
+//  Homeowner
+//
+//  Created by Mathew Wong on 8/24/14.
+//  Copyright (c) 2014 Mathew Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomeownerTests/HomeownerTests.m
@@ -0,0 +1,34 @@
+//
+//  HomeownerTests.m
+//  HomeownerTests
+//
+//  Created by Mathew Wong on 8/24/14.
+//  Copyright (c) 2014 Mathew Wong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HomeownerTests : XCTestCase
+
+@end
+
+@implementation HomeownerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
